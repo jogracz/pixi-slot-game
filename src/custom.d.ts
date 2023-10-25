42172eb3303b612f1a4f46d9a8f63b9231ac8628
Joanna Graczyk <joanna.graczyk1@gmail.com>
2023-10-25 21:41:56 +0200

    Install howler and add sounds

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,3 +2,13 @@ declare module "*.png" {
   const value: any;
   export = value;
 }
+
+declare module "*.mp3" {
+  const value: any;
+  export = value;
+}
+
+declare module "*.wav" {
+  const value: any;
+  export = value;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|mp3|wav)$/i,
         type: "asset/resource",
       },
       {
