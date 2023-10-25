f561ac82f6172fe70fbdd0c5b18a18dae1730c98
Joanna Graczyk <joanna.graczyk1@gmail.com>
2023-10-25 17:18:50 +0200

    Center Screen

--- src/components/Screen.ts
+++ src/components/Screen.ts
@@ -1,3 +1,4 @@
+import * as PIXI from "pixi.js";
 import { GameInterface } from "./Game";
 import Reel, { ReelInterface } from "./Reel";
 
@@ -6,6 +7,8 @@ export interface ScreenInterface {
   reels: ReelInterface[];
   numberOfReels: number;
   isSpinning: boolean;
+  container: PIXI.Container;
+
   update(delta: number): void;
   spin(): void;
 }
@@ -15,12 +18,18 @@ class Screen implements ScreenInterface {
   reels: ReelInterface[];
   numberOfReels: number;
   isSpinning: boolean;
+  container: PIXI.Container;
 
   constructor(game: GameInterface, numberOfReels: number) {
     this.game = game;
     this.reels = [];
     this.numberOfReels = numberOfReels;
     this.isSpinning = false;
+    this.container = new PIXI.Container();
+    this.container.x =
+      (this.game.config.gameWidth - this.game.config.screenWidth) / 2;
+
+    this.game.app.stage.addChild(this.container);
 
     this.createReels();
   }
@@ -36,6 +45,9 @@ class Screen implements ScreenInterface {
       const y = 0;
       this.reels.push(new Reel(this.game, x, y));
     }
+    this.reels.forEach((reel) => {
+      this.container.addChild(reel.container);
+    });
   }
   spin() {
     console.log("SPINNING from Screen");
