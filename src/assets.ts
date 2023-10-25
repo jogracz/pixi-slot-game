fd21f3a5c3005e21a94b3c57d297545858e85ae4
Joanna Graczyk <joanna.graczyk1@gmail.com>
2023-10-25 13:50:09 +0200

    Add assets loading

---
+++ src/assets.ts
@@ -0,0 +1,81 @@
+import s9 from "./assets/9.png";
+import s9_connect from "./assets/9_connect.png";
+import s10 from "./assets/10.png";
+import s10_connect from "./assets/10_connect.png";
+import A from "./assets/A.png";
+import A_connect from "./assets/A_connect.png";
+import J from "./assets/J.png";
+import J_connect from "./assets/J_connect.png";
+import K from "./assets/K.png";
+import K_connect from "./assets/K_connect.png";
+import Q from "./assets/Q.png";
+import Q_connect from "./assets/Q_connect.png";
+import H1 from "./assets/H1.png";
+import H1_connect from "./assets/H1_connect.png";
+import H2 from "./assets/H2.png";
+import H2_connect from "./assets/H2_connect.png";
+import H3 from "./assets/H3.png";
+import H3_connect from "./assets/H3_connect.png";
+import H4 from "./assets/H4.png";
+import H4_connect from "./assets/H4_connect.png";
+import H5 from "./assets/H5.png";
+import H5_connect from "./assets/H5_connect.png";
+import H6 from "./assets/H6.png";
+import H6_connect from "./assets/H6_connect.png";
+import M1 from "./assets/M1.png";
+import M1_connect from "./assets/M1_connect.png";
+import M2 from "./assets/M2.png";
+import M2_connect from "./assets/M2_connect.png";
+import M3 from "./assets/M3.png";
+import M3_connect from "./assets/M3_connect.png";
+import M4 from "./assets/M4.png";
+import M4_connect from "./assets/M4_connect.png";
+import M5 from "./assets/M5.png";
+import M5_connect from "./assets/M5_connect.png";
+import M6 from "./assets/M6.png";
+import M6_connect from "./assets/M6_connect.png";
+
+export interface AssetsInterface {
+  [key: string]: string;
+}
+
+const assets: AssetsInterface = {
+  s9,
+  s9_connect,
+  s10,
+  s10_connect,
+  A,
+  A_connect,
+  J,
+  J_connect,
+  K,
+  K_connect,
+  Q,
+  Q_connect,
+  H1,
+  H1_connect,
+  H2,
+  H2_connect,
+  H3,
+  H3_connect,
+  H4,
+  H4_connect,
+  H5,
+  H5_connect,
+  H6,
+  H6_connect,
+  M1,
+  M1_connect,
+  M2,
+  M2_connect,
+  M3,
+  M3_connect,
+  M4,
+  M4_connect,
+  M5,
+  M5_connect,
+  M6,
+  M6_connect,
+};
+
+export default assets;
--- src/components/Game.ts
+++ src/components/Game.ts
@@ -1,5 +1,5 @@
 import * as PIXI from "pixi.js";
-
+import assets from "../assets";
 import { ConfigInterface } from "../config";
 import Screen, { ScreenInterface } from "./Screen";
 
@@ -8,6 +8,9 @@ export interface GameInterface {
   config: ConfigInterface;
   screen: ScreenInterface;
   isSpinning: boolean;
+
+  assets?: {};
+
   // spinButton: PIXI.Sprite;
   update(delta: number): void;
 }
@@ -16,8 +19,11 @@ class Game implements GameInterface {
   app: PIXI.Application<HTMLCanvasElement>;
   config: ConfigInterface;
   screen: ScreenInterface;
+
   isSpinning: boolean;
   // spinButton: PIXI.Sprite;
+
+  assets?: {};
 
   constructor(
     app: PIXI.Application<HTMLCanvasElement>,
@@ -26,10 +32,18 @@ class Game implements GameInterface {
     this.app = app;
     this.config = config;
     this.screen = new Screen(this, config.numberOfReels);
+    this.assets = undefined;
     this.isSpinning = false;
     // this.spinButton = undefined
 
+    this.loadAssets();
+
     console.log("Hello, it's the Game!");
+  }
+  async loadAssets() {
+    console.log("Loading assets...");
+    PIXI.Assets.addBundle("assets", assets);
+    this.assets = await PIXI.Assets.loadBundle("assets");
   }
   update(delta: number) {}
   spin() {
